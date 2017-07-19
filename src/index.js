ba8f7e0b012e195b1f354cd48f545c2207437438
Namuun Bayaraa <namuka@gmail.com>
2017-07-19 15:12:31 -0700

    Delete extra

--- src/index.js
+++ src/index.js
@@ -2,14 +2,3 @@ import './main.css';
 import { Main } from './Main.elm';
 
 Main.embed(document.getElementById('root'));
-
-var http = require('http');
-
-http.createServer(function (request, response) {
-response.writeHead(200, {
-    'Content-Type': 'text/plain',
-    'Access-Control-Allow-Origin' : '*',
-    'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE'
-});
-response.end('Hello World\n');
-}).listen(3000);
